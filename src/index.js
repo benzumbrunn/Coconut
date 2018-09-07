3e7542060c7134e2862782d8429924fc57d82c64
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-09-07 15:50:11 +0200

    Babel 7 setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Hello, world!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
